e1e2e60bcb53453afcfdfb88fe5dad29844ef5de
Bida3443 <alexanderbamise@gmail.com>
2025-12-29 09:05:57 -0800

    my dental code

--- app/src/component/section5.tsx
+++ app/src/component/section5.tsx
@@ -14,9 +14,9 @@ export default function SectionFive () {
         <div className="flex flex-col justify-center items-center font-bold bg-white ">
             <h1 className="text-black text-center lg:text-[40px] text-[20px] m-10">Meet The Dentist In Lagos</h1>
             
-            <div className="flex flex-row justify-center px-4 w-full gap-4">
+            <div className="flex flex-row justify-center px-4 w-full lg:space-x-20 space-x-10">
                 <motion.div 
-                          initial={{ y: -100, opacity: 0 }}
+                          initial={{ y: -40, opacity: 0 }}
                           whileInView={{ y: 0, opacity: 1 }}
                           transition={{ duration: 0.9, ease: "easeInOut" }}
                   // viewport={{ once: true }}
@@ -33,12 +33,12 @@ export default function SectionFive () {
                     />   
                     <div className="absolute inset-0 bg-black/50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-500 font-bold text-[16px] sm:text-[18px] lg:text-[20px]">
                       
-                      <a className="hover:underline text-center" href="/about" >Dr.Kehinde Bamise<br/>CEO</a>
+                    <a className="hover:underline text-center" href="/about" >Dr.Kehinde Bamise<br/>CEO</a>
                       
                     </div> 
             </motion.div> 
-            <motion.div 
-                    initial= {{ y: 100, opacity: 0 }}
+            <motion.div   
+                    initial= {{ y: 40, opacity: 0 }}
                     whileInView={{ y: 0, opacity: 1 }}
                     transition={{duration: 0.8, ease: "easeInOut"}}
             className="relative group overflow-hidden rounded-xl w-1/2 sm:w-[220px] md:w-[280px] lg:w-[500px]">
