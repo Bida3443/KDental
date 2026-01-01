4741361c82bdbbfe9428f05c4b7ffd4777c8f0c7
Bida3443 <alexanderbamise@gmail.com>
2026-01-01 03:49:01 -0800

    my dental code

--- app/braces/question.tsx
+++ app/braces/question.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {motion} from "framer-motion"
+// import {motion} from "framer-motion"
 
 import { useState } from "react";
 
@@ -50,19 +50,19 @@ const faqs = [
 export default function SectionnTTwo() {
   const [openIndex, setOpenIndex] = useState<number | null>(0);
   return (
-    <div className="px-4 sm:px-6 lg:px-0">
-    <div className="w-full flex justify-center py-10 bg-amber-50">
+    <div className="px-4 sm:px-0 lg:px-0">
+    <div className="w-full flex justify-center p-10 bg-amber-50">
       <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-2xl">
-        <motion.h2 
-        initial={{y: 80, opacity: 0}}
-        animate={{y: 0, opacity: 1}}
-        transition={{duration: 0.8, ease: "easeOut"}}
-        viewport={{once: true}}
+        <h2 
+        // initial={{y: 80, opacity: 0}}
+        // animate={{y: 0, opacity: 1}}
+        // transition={{duration: 0.8, ease: "easeOut"}}
+        // viewport={{once: true}}
         
         
         className="text-center text-2xl font-semibold mb-6 text-blue-700">
           Cosmetics Braces Most Popular FAQs
-        </motion.h2>
+        </h2>
 
         {faqs.map((faq, index) => (
           <div key={index} className="mb-3">
