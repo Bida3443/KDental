0febb3b2510aa359d8c56c7b82cf8d8e30c90b45
Bida3443 <alexanderbamise@gmail.com>
2026-01-01 03:32:10 -0800

    my dental code

--- app/braces/section2.tsx
+++ app/braces/section2.tsx
@@ -9,7 +9,7 @@ export default function BeforeAfter() {
     return(
         <div className="flex flex-col justify-center items-center p-5 gap-10 bg-white pb-10">
             <div>
-                <h1 className="text-4xl font-bold text-blue-700">Braces: Before, Progress and After</h1>
+                <h1 className="lg:text-4xl text-2x1 font-bold text-blue-700">Braces: Before, Progress and After</h1>
                 <div className="border-2 border-blue-700 w-50 mx-auto M-5 flex items-center mt-5 justify-center"></div>
 
             </div>
