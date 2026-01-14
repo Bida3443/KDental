5b5c248b8e93dbf13fc770a2137c205788768feb
Bida3443 <alexanderbamise@gmail.com>
2026-01-14 03:15:50 -0800

    my dental code

--- app/braces/emblacarousel.tsx
+++ app/braces/emblacarousel.tsx
@@ -42,8 +42,16 @@ export default function EmblaCarousel() {
 
   return (
     <div className="relative w-full overflow-hidden">
-      <div>
-        <h1 className="bg-red-600">Google Review</h1>
+      <Image
+      src={review5}
+      alt="Backgrond image"
+      fill
+      className="w-full h-full opacity-10 z-20"
+      style={{objectFit:"cover"}}
+      />
+      <div className="absolute w-full h-full bg-blue-400 " />
+      <div className="relative p-5">
+        <h1 className="bg-red-600 flex text-black justify-center font-bold text-[30px]">Google Review</h1>
       </div>
       {/* Viewport */}
       <div ref={emblaRef} className="overflow-hidden">
