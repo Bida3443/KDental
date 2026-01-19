7ff8f7fd885da5e62cbecebcd19cbe6f502055f4
Bida3443 <alexanderbamise@gmail.com>
2026-01-19 05:03:00 -0800

    my dental code

--- app/service/section1.tsx
+++ app/service/section1.tsx
@@ -85,7 +85,7 @@ export default function SecttionOnne() {
         </svg>
       ),
       text: "Teeth Whitening",
-      link: "#",
+      link: "whitening",
     },
 
     // Another one
---
+++ app/whitening/page.tsx
@@ -0,0 +1,16 @@
+import HeroWhite from "./hero";
+import ImageTextRow from "../src/component/info";
+import NavBar from "../src/component/Nav";
+
+
+export default function Whitening () {
+
+    return(
+        <div>
+            <ImageTextRow/>
+            <NavBar/>
+            <HeroWhite/>
+
+        </div>
+    )
+}
